464bb1960c67960bae7d0ce2f7cc95d2287338a2
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-05-25 20:09:48 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/ProjectPreparation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numProjects = int.Parse(Console.ReadLine());
+            int time = numProjects * 3;
+
+            Console.WriteLine($"The architect {name} will need {time} hours to complete {numProjects} project/s.");
+        }
+    }
+}

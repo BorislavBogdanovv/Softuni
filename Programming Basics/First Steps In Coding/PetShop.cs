415eefec030dbe6111f3582b23dd86783adc9ca2
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-05-25 20:21:05 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/PetShop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDog = int.Parse(Console.ReadLine());
+            int numCat = int.Parse(Console.ReadLine());
+
+            double priceDog = numDog * 2.5;
+            int priceCat = numCat * 4;
+
+            double sum = priceDog + priceCat;
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}

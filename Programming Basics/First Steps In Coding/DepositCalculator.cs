e4b0c9f68aa05e24751856fcf309f708418598cd
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-05-27 12:08:41 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/DepositCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dep = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            double perc = double.Parse(Console.ReadLine());
+
+            double inter = dep * (perc / 100);
+            double month = inter / 12;
+
+            double sum = dep + term * month;
+            Console.WriteLine(sum);
+        }
+    }
+}

02b885cedeba22025eadb6d8b534e6bdcdc9c4ee
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-05-31 15:17:02 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/VacationBooksList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numBook = int.Parse(Console.ReadLine());
+            int numPage = int.Parse(Console.ReadLine());
+            int numDays = int.Parse(Console.ReadLine());
+
+            int time = numBook / numPage;
+            int needt = time / numDays;
+
+            Console.WriteLine(needt);
+        }
+    }
+}

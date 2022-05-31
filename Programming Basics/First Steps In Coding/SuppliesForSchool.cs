fcc3c19b4a127daafb7e7daddfb1a4b5ec9cce24
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-05-31 15:18:05 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/SuppliesForSchool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pens = int.Parse(Console.ReadLine());
+            int markers = int.Parse(Console.ReadLine());
+            int preparation = int.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            double pensPrice = pens * 5.80;
+            double markersPrice = markers * 7.20;
+            double preparationPrice = preparation * 1.20;
+
+            double sum = pensPrice + markersPrice + preparationPrice;
+
+            double totalSum = sum - sum * (percent / 100.0);
+            Console.WriteLine(totalSum);
+        }
+    }
+}

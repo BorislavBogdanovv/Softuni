f63f26af4ee9607377386b6160e96d84f522ebdf
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-06-02 11:19:04 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/Aquarium.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double percent = double.Parse(Console.ReadLine());
+
+            double volume = lenght * width * height;
+
+            double volumeLiters = volume / 1000;
+
+            double busy = percent / 100;
+
+            double liters = volumeLiters * (1 - busy);
+
+            Console.WriteLine(liters);
+        }
+    }
+}

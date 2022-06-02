e8c65494a351df402cca7d7e97aeea06ca74a494
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-06-02 11:18:53 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/BasketballEquipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = double.Parse(Console.ReadLine());
+
+            double shoesPrice = tax - tax * (40 / 100.0);
+            double outfitPrice = shoesPrice - shoesPrice * (20 / 100.0);
+            double ballPrice = outfitPrice / 4;
+            double accesPrice = ballPrice / 5;
+
+            double sum = tax + shoesPrice + outfitPrice + ballPrice + accesPrice;
+            Console.WriteLine(sum);
+        }
+    }
+}

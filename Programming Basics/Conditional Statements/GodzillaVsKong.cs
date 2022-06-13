3cbaaeff8f77153c4d7fa7d6d53ef031df51626c
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-06-13 19:10:47 +0300

    Add files via upload

---
+++ Programming Basics/Conditional Statements/GodzillaVsKong.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int extra = int.Parse(Console.ReadLine());
+            double priceExCloth = double.Parse(Console.ReadLine());
+
+            double priceDecor = budget * 0.1;
+            double totalPriceExCloth = extra * priceExCloth;
+
+            if (extra > 150)
+            {
+                totalPriceExCloth = totalPriceExCloth * 0.9;
+            }
+
+            double priceFilm = priceDecor + totalPriceExCloth;
+            double restMoney = budget - priceFilm;
+
+            if (priceDecor + totalPriceExCloth <= budget)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {restMoney:f2} leva left.");
+            }
+
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(restMoney):f2} leva more.");
+            }
+
+
+        }
+    }
+}

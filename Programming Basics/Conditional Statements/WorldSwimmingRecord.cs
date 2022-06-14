aaaedfd8d555593b6cda139c8fff0a361da2c583
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-06-14 18:58:53 +0300

    Add files via upload

---
+++ Programming Basics/Conditional Statements/WorldSwimmingRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double secRecord = double.Parse(Console.ReadLine());
+            double mDistance = double.Parse(Console.ReadLine());
+            double secTime1M = double.Parse(Console.ReadLine());
+
+            double secDistance = mDistance * secTime1M;
+            double M15 = mDistance / 15;
+            double sec125 = Math.Floor(M15) * 12.5;
+
+            double totalTime = secDistance + sec125;
+
+            if (secRecord <= totalTime)
+            {
+                Console.WriteLine($"No, he failed! He was {(totalTime - secRecord):f2} seconds slower.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");   
+            }
+        }
+    }
+}

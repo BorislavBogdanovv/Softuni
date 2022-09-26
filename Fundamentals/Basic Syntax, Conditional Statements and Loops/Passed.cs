5c119608e09920c4362fffce4c314f0435deb434
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-09-26 16:50:40 +0300

    Create Passed.cs

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops/Passed.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}

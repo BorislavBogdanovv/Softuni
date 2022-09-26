fbf7083eace810f806416a14244258ad550e73dc
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-09-26 16:59:24 +0300

    Create BackIn30Minutes.cs

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops/BackIn30Minutes.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace BackIn30Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine()) + 30;
+
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}

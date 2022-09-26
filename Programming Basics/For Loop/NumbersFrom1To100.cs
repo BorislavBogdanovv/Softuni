5ef20337d4aa862634c14120cd1f96a69f97971b
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-09-26 17:13:18 +0300

    Create NumbersFrom1To100.cs

---
+++ Programming Basics/For Loop/NumbersFrom1To100.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

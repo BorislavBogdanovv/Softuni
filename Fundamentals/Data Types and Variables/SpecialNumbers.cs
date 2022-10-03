ab5a20b3d3bf6b7a759e7d6402a61d994b83ce07
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-10-03 11:35:20 +0300

    Create SpecialNumbers.cs

---
+++ Fundamentals/Data Types and Variables/SpecialNumbers.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum = i % 10;
+                sum += i / 10;
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+
+            }
+        }
+    }
+}

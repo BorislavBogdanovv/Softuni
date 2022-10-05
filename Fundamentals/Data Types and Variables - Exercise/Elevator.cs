9fe306a05c0c39ff8e4b0cb15dfa9007978eb7f2
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-10-05 11:01:36 +0300

    Create Elevator.cs

---
+++ Fundamentals/Data Types and Variables - Exercise/Elevator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = 0;
+
+            courses = people / capacity;
+
+            if (people % capacity != 0)
+            {
+                courses++;
+            }
+
+            Console.WriteLine(courses);
+        }
+    }
+}

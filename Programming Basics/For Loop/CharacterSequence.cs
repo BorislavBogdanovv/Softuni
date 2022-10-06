6870d22296ffaba6bd822031efdd24c2b436fa36
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-10-06 10:11:49 +0300

    Create CharacterSequence.cs

---
+++ Programming Basics/For Loop/CharacterSequence.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace EvenPowersTo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}

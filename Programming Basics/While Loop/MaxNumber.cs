bfd2289efb6f174551b1d7e0ed687e97f08e2b0a
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-10-24 11:39:20 +0300

    Create MaxNumber.cs

---
+++ Programming Basics/While Loop/MaxNumber.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int max = int.MinValue;
+            
+
+            while (command != "Stop")
+            {
+                int num = int.Parse(command);
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}

b63f759db7bf93fbe81520ae3e3071c45c333dff
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-10-25 10:17:56 +0300

    Create OldBooks.cs

---
+++ Programming Basics/While Loop/OldBooks.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace OldBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int counter = 0;
+            bool isBookFound = false;
+
+            string nextBook = Console.ReadLine();
+
+            while (nextBook != "No More Books")
+            {
+                if (nextBook == book)
+                {
+                    isBookFound = true;
+                    break;
+                }
+
+                counter++;
+                nextBook = Console.ReadLine();
+            }
+
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}

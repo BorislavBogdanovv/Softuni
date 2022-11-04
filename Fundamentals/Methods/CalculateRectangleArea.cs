4e1a224e43825fdb3ce8886a014a4a45a5e4f70d
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-11-04 14:52:58 +0200

    Create CalculateRectangleArea.cs

---
+++ Fundamentals/Methods/CalculateRectangleArea.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double area = GetRectangleArea(height, width);
+            Console.WriteLine(area);
+        }
+
+        static double GetRectangleArea(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}

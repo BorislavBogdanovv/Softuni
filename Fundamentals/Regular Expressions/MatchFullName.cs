1d2859fe6018d8780d085667a440fb08d9011480
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-12-12 09:50:36 +0200

    Create MatchFullName.cs

---
+++ Fundamentals/Regular Expressions/MatchFullName.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _30._Lab_Regular_Expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            foreach (Match item in matchCollection)
+            {
+                Console.Write(item.Value + " ");
+            }
+        }
+    }
+}

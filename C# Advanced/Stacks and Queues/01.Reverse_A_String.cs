1cd8ef22fa7873d15b48689226daa2615e14982e
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2023-01-12 12:22:57 +0200

    Create 01.Reverse_A_String.cs

---
+++ C# Advanced/Stacks and Queues/01.Reverse_A_String.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace _01.StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var character in input)
+            {
+                stack.Push(character);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}

ec01174598e120e02da7be666dadf6c2ef7c78a3
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2023-02-02 12:00:21 +0200

    Create 03.MaximalSum.cs

---
+++ C# Advanced/Multidimensional Arrays - Exercise/03.MaximalSum.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+
+int[] dimensions = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = dimensions[0];
+int cols = dimensions[1];
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = numbers[col];
+    }
+}
+
+int maxSum = int.MinValue;
+int maxSumRow = 0;
+int maxSumCol = 0;
+
+for (int row = 0; row < rows - 2; row++)
+{
+    for (int col = 0; col < cols - 2; col++)
+    {
+        int currSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+        matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+        matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+        if (currSum > maxSum)
+        {
+            maxSum = currSum;
+            maxSumRow = row;
+            maxSumCol = col;
+        }
+    }
+}
+
+Console.WriteLine($"Sum = {maxSum}");
+
+for (int row = maxSumRow; row < maxSumRow + 3; row++)
+{
+    for (int col = maxSumCol; col < maxSumCol + 3; col++)
+    {
+        Console.Write($"{matrix[row, col]} ");
+    }
+
+    Console.WriteLine();
+}

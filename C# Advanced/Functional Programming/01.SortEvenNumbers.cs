24774ca26ca08484909a592a0abb0d8233ccea02
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2023-03-01 11:20:39 +0200

    Create 01.SortEvenNumbers.cs

---
+++ C# Advanced/Functional Programming/01.SortEvenNumbers.cs
@@ -0,0 +1,6 @@
+int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse)
+    .Where(x => x % 2 == 0)
+    .OrderBy(x => x)
+    .ToArray();
+
+Console.WriteLine(String.Join(", ", numbers));

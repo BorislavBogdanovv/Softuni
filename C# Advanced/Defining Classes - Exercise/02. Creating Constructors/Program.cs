52b7d83f3664ffb0e1ebf62d58b1ab6c4386e6f6
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2023-03-08 11:47:39 +0200

    Add files via upload

---
+++ C# Advanced/Defining Classes - Exercise/02. Creating Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person(18);
+
+        Console.WriteLine($"{person.Name} is {person.Age} years old");
+    }
+}
